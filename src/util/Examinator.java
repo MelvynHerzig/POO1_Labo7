f3a6635535110c86c1defe04148e8154bab97288
MelvynHerzig <34660483+MelvynHerzig@users.noreply.github.com>
2020-11-11 20:45:33 +0100

    Pile et tests

--- src/util/Examinator.java
+++ src/util/Examinator.java
@@ -1,20 +1,38 @@
 package util;
 
+/**
+ * Classe implémentant l'équivalent des itérateur sur la classe Stack
+ *
+ * @author Forestier Quentin, Herzig Melvyn
+ * @version 11.11.2020
+ */
 public class Examinator
 {
     private Element elem;
 
+    /**
+     * Constructeur.
+     * @param elem Element sur lequel pointer.
+     */
     Examinator(Element elem)
     {
         this.elem = elem;
     }
 
+    /**
+     * Avance l'itérateur est retourne le contenu de l'Element observé.
+     * @return Contenu du nouvel Element.
+     */
     public Object next()
     {
         elem = elem.next;
         return elem.value;
     }
 
+    /**
+     * Vérifie si l'Element pointé par l'itérateur est le dernier.
+     * @return Faux si des Element suivent.
+     */
     public boolean hasNext()
     {
         return elem.next != null;
--- src/util/Stack.java
+++ src/util/Stack.java
@@ -1,38 +1,63 @@
 package util;
 
+/**
+ * Classe implémentant le fonctionnement d'une pile LIFO.
+ *
+ * @author Forestier Quentin, Herzig Melvyn
+ * @version 11.11.2020
+ */
 public class Stack
 {
     private int size;
     private Element head;
 
+    /**
+     * Constructeur de pile.
+     */
     public Stack()
     {
         head = null;
         size = 0;
     }
 
+    /**
+     * Fonction de récupération du haut de la pile.
+     * @return Retourne le sommet de la pile.
+     * @throws RuntimeException si liste vide
+     */
     public Object top()
     {
-        if(isEmpty()) throw new RuntimeException();
+        if(isEmpty()) throw new RuntimeException("top on empty stack");
 
         return head.value;
     }
 
+    /**
+     * Supprime l'élément en haut de la pile.
+     * @throws RuntimeException si liste vide
+     */
     public void pop()
     {
-        if(isEmpty()) throw new RuntimeException();
+        if(isEmpty()) throw new RuntimeException("pop on empty stack");
 
         head = head.next;
         size--;
     }
 
+    /**
+     * Ajoute un élément en haut de la pile.
+     * @param o Élément à ajouter en haut de la pile.
+     */
     public void push(Object o)
     {
         head = new Element(o, head);
         size++;
     }
 
-    @Override
+    /**
+     * Convertit la pile en chaîne de caractères.
+     * @return Pile convertie en chaîne de caractères.
+     */
     public String toString()
     {
         StringBuilder data = new StringBuilder();
@@ -45,6 +70,10 @@ public class Stack
         return String.format("[ %s]", data.toString());
     }
 
+    /**
+     * Converti les éléments de la pile en un tableau.
+     * @return Tableau contenant les éléments de la pile. 0 = haut de la pile.
+     */
     public Object[] toArray()
     {
         if(isEmpty()) return new Object[0];
@@ -62,16 +91,27 @@ public class Stack
         return tbl;
     }
 
+    /**
+     * Crée et retourne un examinateur sur le début de la pile.
+     * @return Examinateur sur le début de la pile, regarde null si pile vide.
+     */
     public Examinator examinator()
     {
-        return new Examinator(head);
+        return new Examinator(new Element(null, head));
     }
 
+    /**
+     * Vérifie si la pile est vide.
+     * @return Vrai si la pile est vide.
+     */
     public boolean isEmpty()
     {
         return size == 0;
     }
 
+    /*
+     * @return Renvoie la taille de la pile
+     */
     public int size()
     {
         return size;
