a4a35a89c1c16bb2cbbb8a61b5ed58a4eb0c76a9
Forestier <quentin.forestier@heig-vd.ch>
2020-11-19 14:03:17 +0100

    la ça devrait être bon

--- src/hanoi/Hanoi.java
+++ src/hanoi/Hanoi.java
@@ -1,11 +1,9 @@
-import util.Stack;
-
 package hanoi;
 
 import util.Stack;
 
 /**
- * Classe permettant la résolution du problème des tours d'Hanoi.
+ * Classe permettant la résolution du problème des tours d'hanoi.Hanoi.
  *
  * @author Forestier Quentin, Herzig Melvyn
  * @version 18.11.2020
@@ -22,19 +20,19 @@ public class Hanoi
     private final Stack s3;
 
 
-    private HanoiDisplayer displayer;
+    private final HanoiDisplayer displayer;
 
     /**
      * Constructeur générique de la classe
      *
      * @param disks     Nombre de disques
-     * @param displayer Objet permettant l'affichage des aiguilles d'Hanoi
+     * @param displayer Objet permettant l'affichage des aiguilles d'hanoi.Hanoi
      */
     public Hanoi(int disks, HanoiDisplayer displayer)
 
     {
         this.turnCounter = 0;
-        this.diskNumber = aDiskNumber;
+        this.diskNumber = disks;
 
         this.displayer = displayer;
 
@@ -62,141 +60,131 @@ public class Hanoi
     }
 
 
-    public void nextIteration()
+    /**
+     * Déplace les disques de la première à la troisième aiguille.
+     * Affiche toutes les étapes grâce à un HanoiDisplayer.
+     */
+    public void solve()
     {
 
-        turnCounter++;
-        switch (turnCounter % 3)
+        Stack inter = diskNumber % 2 == 0 ? s3 : s2;
+        Stack dest = diskNumber % 2 == 0 ? s2 : s3;
+
+
+        displayer.display(this);
+
+        while (!finished())
         {
-            case 0:
-                transfert(s3, s2);
-                break;
-            case 1:
-                transfert(s1, s2);
-                break;
-            case 2:
-                transfert(s1, s3);
-                break;
-
-            /**
-             * Déplace les disques de la première à la troisième aiguille.
-             * Affiche toutes les étapes grâce à un HanoiDisplayer.
-             */
-            public void solve ()
+            turnCounter++;
+            switch (turnCounter % 3)
             {
-
-                Stack inter = diskNumber % 2 == 0 ? s3 : s2;
-                Stack dest = diskNumber % 2 == 0 ? s2 : s3;
-
-
-                displayer.display(this);
-
-                while (!finished())
-                {
-                    turnCounter++;
-                    switch (turnCounter % 3)
-                    {
-                        case 0:
-                            transfert(inter, dest);
-                            break;
-                        case 1:
-                            transfert(s1, dest);
-                            break;
-                        case 2:
-                            transfert(s1, inter);
-                            break;
-                    }
-                    displayer.display(this);
-                }
-
+                case 0:
+                    transfer(inter, dest);
+                    break;
+                case 1:
+                    transfer(s1, dest);
+                    break;
+                case 2:
+                    transfer(s1, inter);
+                    break;
             }
+            displayer.display(this);
+        }
 
-            /**
-             * Rend un tableau de tableaux représentant l'état des 3 aiguilles.
-             * @return Tableau de tableaux de int contenant l'état des
-             * aiguilles.
-             */
-            public int[][] status ()
+    }
+
+    /**
+     * Rend un tableau de tableaux représentant l'état des 3 aiguilles.
+     *
+     * @return Tableau de tableaux de int contenant l'état des
+     * aiguilles.
+     */
+    public int[][] status()
+    {
+        int[][] t = new int[3][];
+
+        Stack[] tbl = new Stack[]{s1, s2, s3};
+
+        for (int i = 0; i < tbl.length; i++)
+        {
+            t[i] = new int[tbl[i].size()];
+            Object[] tmp = tbl[i].toArray();
+
+            for (int j = 0; j < tbl[i].size(); j++)
             {
-                int[][] t = new int[3][];
-
-                Stack[] tbl = new Stack[]{s1, s2, s3};
-
-                for (int i = 0; i < tbl.length; i++)
-                {
-                    t[i] = new int[tbl[i].size()];
-                    Object[] tmp = tbl[i].toArray();
-
-                    for (int j = 0; j < tbl[i].size(); j++)
-                    {
-                        t[i][j] = (int) tmp[j];
-                    }
-                }
-
-
-                return t;
-            }
-
-            /**
-             * Permet de savoir si tous les disques sont sur la troisième
-             * aiguille.
-             * @return True si la solution est atteinte, False sinon
-             */
-            public boolean finished ()
-            {
-                return s1.isEmpty() && s2.isEmpty() && maxTurn == turnCounter;
-            }
-
-            /**
-             * Nombre de fois qu'un disque a été déplacé.
-             * @return Compteur du nombre d'itération.
-             */
-            public int turn ()
-            {
-                return maxTurn == turnCounter;
-            }
-
-            /**
-             * Convertit l'objet Hanoi en chaine de caractère
-             * @return
-             */
-            @Override
-            public String toString ()
-            {
-                return String.format("" +
-                        "-- Turn : %d\n" +
-                        "One  : %s\n" +
-                        "Two  : %s\n" +
-                        "Three: %s", turnCounter, s1, s2, s3);
-            }
-
-            /**
-             *
-             * @param src
-             * @param dest
-             */
-            private void transfert (Stack src, Stack dest)
-            {
-
-                if (src.isEmpty())
-                {
-                    src.push(dest.top());
-                    dest.pop();
-                }
-                else if (dest.isEmpty())
-                {
-                    dest.push(src.top());
-                    src.pop();
-                }
-                else if ((int) src.top() > (int) dest.top())
-                {
-                    src.push(dest.top());
-                    dest.pop();
-                }
-                else
-                {
-                    dest.push(src.top());
-                    src.pop();
-                }
+                t[i][j] = (int) tmp[j];
             }
         }
+
+
+        return t;
+    }
+
+    /**
+     * Permet de savoir si tous les disques sont sur la troisième
+     * aiguille.
+     *
+     * @return True si la solution est atteinte, False sinon
+     */
+    public boolean finished()
+    {
+        return s1.isEmpty() && s2.isEmpty() && maxTurn == turnCounter;
+    }
+
+    /**
+     * Nombre de fois qu'un disque a été déplacé.
+     *
+     * @return Compteur du nombre d'itération.
+     */
+    public int turn()
+    {
+        return turnCounter;
+    }
+
+    /**
+     * Convertit l'objet hanoi.Hanoi en chaine de caractère
+     *
+     * @return Hanoi convertit en chaine de caractère
+     */
+    @Override
+    public String toString()
+    {
+        return String.format("" +
+                "-- Turn : %d\n" +
+                "One  : %s\n" +
+                "Two  : %s\n" +
+                "Three: %s", turnCounter, s1, s2, s3);
+    }
+
+    /**
+     * Transfert l'élément le plus petit entre les 2 sommets des piles sur la
+     * bonne pile
+     *
+     * @param src
+     * @param dest
+     */
+    private void transfer(Stack src, Stack dest)
+    {
+
+        if (src.isEmpty())
+        {
+            src.push(dest.top());
+            dest.pop();
+        }
+        else if (dest.isEmpty())
+        {
+            dest.push(src.top());
+            src.pop();
+        }
+        else if ((int) src.top() > (int) dest.top())
+        {
+            src.push(dest.top());
+            dest.pop();
+        }
+        else
+        {
+            dest.push(src.top());
+            src.pop();
+        }
+    }
+}
